2acf0c1f0d35e84c2dfea9817291f79065dc4c17
elleg <119740899@umail.ucc.ie>
2021-12-05 19:25:44 +0000

    Added in files for testing

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,13 +2,25 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField email;
+    public TextField password;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        String userEmail = email.getText();
+        String userPass = password.getText();
+
+        boolean b = userPass.length() < 7;
+        if (b = true) {
+            welcomeText.setText("Password must be longer than 7 characters.");
+        }
+        else {
+            welcomeText.setText("");
     }
-}
+}}
