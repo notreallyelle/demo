901b6fa9a23a9769740ceef159593fcc19b4f4fc
elleg <119740899@umail.ucc.ie>
2021-12-05 18:48:05 +0000

    Added in text fields and buttons for the email and password

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
